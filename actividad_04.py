684b6c40ad8b8ff48bdbe20b247f13832b7f9724
Y.V <straysheep753@gmail.com>
2025-07-18 09:13:38 -0600

    2. First function idea created, and print of the students compendium

--- actividad_04.py
+++ actividad_04.py
@@ -1,4 +1,12 @@
 key = True
+class CollegeStudent:
+    def __init__(self, name, age, course, avg_score):
+        self.name = name
+        self.age = age
+        self.course = course
+        self.avg_score = avg_score
+
+students_compendium = {}
 while key:
     menu_inpt = input("----------Bienvenido----------\n"
                       "\n1. Agregar estudiante"
@@ -7,9 +15,11 @@ while key:
                       "\n4. Salir")
     match menu_inpt:
         case "1":
-            pass
+            temp_student_name = input("Coloque el nombre del estudiante: ")
+            temp_student_avg_score = input("Coloque su promedio: ")
+            students_compendium.update({temp_student_name:temp_student_avg_score})
         case "2":
-            pass
+            print(students_compendium)
         case "3":
             pass
         case "4":

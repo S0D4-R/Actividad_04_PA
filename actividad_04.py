7abf45ad0e57322391ff86e27c294944102b56e7
Y.V <straysheep753@gmail.com>
2025-07-18 09:04:01 -0600

    1. First impression: Menu

---
+++ actividad_04.py
@@ -0,0 +1,17 @@
+key = True
+while key:
+    menu_inpt = input("----------Bienvenido----------\n"
+                      "\n1. Agregar estudiante"
+                      "\n2.Mostrar lista de estudiantes"
+                      "\n3. Calcular promedio general"
+                      "\n4. Salir")
+    match menu_inpt:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            print("Gracias por usar el programa")
+            key = False
